c5d7e0cff10310f6d1589617ecb61c5e4081d330
Peter Bollhorn <pbollhorn@gmail.com>
2025-01-19 15:01:17 +0100

    now uses scanner to get recipient email address

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.sendgrid.Method;
 import com.sendgrid.Request;
@@ -20,23 +21,28 @@ public class Main {
         String SENDGRID_API_KEY = System.getenv("SENDGRID_API_KEY");
         String SENDGRID_TEMPLATE_ID = System.getenv("SENDGRID_TEMPLATE_ID");
 
+        // Indlæs modtager email fra konsollen
+        System.out.println("Indtast modtagerens email:");
+        Scanner scanner = new Scanner(System.in);
+        String toEmailAddress = scanner.nextLine();
+        scanner.close();
+
         Email from = new Email(FROM_EMAIL_ADDRESS);
         from.setName("Byggemarked");
-
-        Mail mail = new Mail();
-        mail.setFrom(from);
-        mail.templateId = SENDGRID_TEMPLATE_ID;
 
         /* Erstat kunde@gmail.com, name, email og zip med egne værdier ****/
         /* I test-fasen - brug din egen email, så du kan modtage beskeden */
         Personalization personalization = new Personalization();
-        personalization.addTo(new Email("anders@and.com"));
+        personalization.addTo(new Email(toEmailAddress));
         personalization.addDynamicTemplateData("name", "Anders And");
-        personalization.addDynamicTemplateData("email", "anders@and.dk");
+        personalization.addDynamicTemplateData("email", toEmailAddress);
         personalization.addDynamicTemplateData("zip", "2100");
-        mail.addPersonalization(personalization);
 
+        Mail mail = new Mail();
+        mail.setFrom(from);
+        mail.addPersonalization(personalization);
         mail.addCategory("carportapp");
+        mail.templateId = SENDGRID_TEMPLATE_ID;
 
         // Klargør request
         SendGrid sg = new SendGrid(SENDGRID_API_KEY);
@@ -47,6 +53,7 @@ public class Main {
             request.setBody(mail.build());
 
             // Send request (dvs. send email) og modtag response
+            // Status code 202 indikerer success
             Response response = sg.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
