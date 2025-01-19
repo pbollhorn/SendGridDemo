40fc5e3ab57060e095682a47a6c1b444d981b4f7
Peter Bollhorn <pbollhorn@gmail.com>
2025-01-19 13:57:10 +0100

    second commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String API_KEY = System.getenv("SENDGRID_API_KEY");
+        System.out.println(API_KEY);
+
     }
+
 }
